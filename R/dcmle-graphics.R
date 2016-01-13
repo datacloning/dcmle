e3440b44fa3f93ce06ea83eff0a4350388e9e220
psolymos <psolymos@gmail.com>
2016-01-12 22:19:46 -0700

    one more internal

--- R/dcmle-graphics.R
+++ R/dcmle-graphics.R
@@ -4,10 +4,48 @@
 function (x, trace = TRUE, density = TRUE, smooth = TRUE, bwf,
     auto.layout = TRUE, ask = par("ask"), ...)
 {
+    .set_mfrow
+    function (Nchains = 1, Nparms = 1, nplots = 1, sepplot = FALSE)
+    {
+    mfrow <- if (sepplot && Nchains > 1 && nplots == 1) {
+            if (Nchains == 2) {
+                switch(min(Nparms, 5), c(1, 2), c(2, 2), c(3, 2),
+                    c(4, 2), c(3, 2))
+            }
+            else if (Nchains == 3) {
+                switch(min(Nparms, 5), c(2, 2), c(2, 3), c(3, 3),
+                    c(2, 3), c(3, 3))
+            }
+            else if (Nchains == 4) {
+                if (Nparms == 1)
+                    c(2, 2)
+                else c(4, 2)
+            }
+            else if (any(Nchains == c(5, 6, 10, 11, 12)))
+                c(3, 2)
+            else if (any(Nchains == c(7, 8, 9)) || Nchains >= 13)
+                c(3, 3)
+        }
+        else {
+            if (nplots == 1) {
+                mfrow <- switch(min(Nparms, 13), c(1, 1), c(1, 2),
+                    c(2, 2), c(2, 2), c(3, 2), c(3, 2), c(3, 3),
+                    c(3, 3), c(3, 3), c(3, 2), c(3, 2), c(3, 2),
+                    c(3, 3))
+            }
+            else {
+                mfrow <- switch(min(Nparms, 13), c(1, 2), c(2, 2),
+                    c(3, 2), c(4, 2), c(3, 2), c(3, 2), c(4, 2),
+                    c(4, 2), c(4, 2), c(3, 2), c(3, 2), c(3, 2),
+                    c(4, 2))
+            }
+        }
+        return(mfrow)
+    }
     oldpar <- NULL
     on.exit(par(oldpar))
     if (auto.layout) {
-        mfrow <- set.mfrow(Nchains = nchain(x), Nparms = nvar(x),
+        mfrow <- .set_mfrow(Nchains = nchain(x), Nparms = nvar(x),
             nplots = trace + density)
         oldpar <- par(mfrow = mfrow)
     }
